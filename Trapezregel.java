3397741b07e3d66b251ac2d3043d6c85dc56fe28
David Edel <davidedel@macbookprode.fritz.box>
2021-11-24 13:22:36 +0100

    added Trapez

---
+++ Trapezregel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Trapezregel {
+    public static void main(String[] args) {
+        
+    }
+    public static double f(double x){
+    if (x>-1){
+        double fx=Math.exp(x)*(1.0/(Math.sqrt(1.0+Math.pow(x,3))))
+            return 0.0;
+    }
+    else{
+        System.out.println("x ist kleinergleich -1");
+        System.exit(1);
+    }
+
+}
+    public static double Trapez(double a, double b){
+        
+
+    }
+}
